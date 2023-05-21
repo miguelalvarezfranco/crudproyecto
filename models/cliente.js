468bb2dc9b1b1ea411e94bdfe95262eab0f7e87b
miguel <maalvarez7441@misena.edu.do>
2023-05-21 10:59:19 -0500

    se comienzan hacer los schemas

--- models/cliente.js
+++ models/cliente.js
@@ -1,5 +1,45 @@
-/*const mongoose = require('../config/conexion');
+const mongoose = require('../config/conexion');
 
 const SchemaCliente = new mongoose.Schema({
-    
-})*/
+    nombre:{
+        type:String,
+        required:true,
+        default:"no registro"
+    },
+    telefono:{
+        type:Number,
+        required:true,
+        min:0,
+        maximo:1000
+
+    },
+    ubicacion:{
+        type:String,
+        required:true,
+
+    },
+    longitud:{
+        type:String,
+        required:true,
+    },
+    latitud:{
+        type:String,
+        required:true,
+    },
+    nivelZoom:{
+        type:String,
+        required:true,
+    },
+    totalComprado:{
+        type:String,
+        required:true,
+    },
+    histoticoDeCompras:{
+        type:String,
+        required:true,
+    }
+
+
+
+
+})
